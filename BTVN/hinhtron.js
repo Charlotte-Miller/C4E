fdfe67aee778bdae9c4f2e6e0d4529e49541afbc
chitauri98 <46620759+chitauri98@users.noreply.github.com>
2020-03-21 12:14:56 +0700

    Add files via upload

---
+++ BTVN/hinhtron.js
@@ -0,0 +1,12 @@
+// Tính diện tích hình tròn//
+// const bankinh = prompt("Bán kính đường tròn");
+// const S = bankinh * bankinh * 3.14; 
+// const name = "Diện tích đường tròn =";
+// const totalString = `${name} ${S}`;
+// console.log(totalString);
+
+// Đổi nhiệt độ//
+const C = prompt("Nhập nhiệt độ cần đổi");
+const CtoF = (C * 9 / 5 + 32);
+const totalString = `${C} (C) = ${CtoF} (F)`;
+console.log(totalString);
