77720d9b777f8fb1be33e9be80263ba8d11dd8a1
chitauri98 <46620759+chitauri98@users.noreply.github.com>
2020-04-14 21:14:56 +0700

    Add files via upload

---
+++ BT Corona2/BTLan2.js
@@ -0,0 +1,45 @@
+//B1
+// let N = Number(prompt("Nhập số N bất kì:"));
+
+// let sum1 = 0;
+// let sum2 = 0;
+// for (let i = 0 ;i <= N; i++ ) {
+//         if (i % 2 == 0) {
+//             sum1 += i;
+//         } else {
+//             sum2 += i;
+//         }
+// }
+// console.log("tổng các số chẵn là trong từ 1 đến " + N + " là " + sum1);
+// console.log("tổng các số lẻ là trong từ 1 đến " + N + " là " + sum2);
+//B2
+// let N = Number(prompt("Nhập số N > 1 bất kì:"));
+
+// let souoc = 0;
+// for (let i = 2; i <= N;i ++ ) {
+//     if (N % i == 0) {
+//         souoc ++;
+//     }
+// }
+// if (souoc == 1) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+//B3
+// let a = Number(prompt("Nhập số N > 1 bất kỳ: "));
+// let string = "";
+// for (let i = 2; i <= a; i++){
+//     let souoc = 0;
+//     for(let j = 1; j <=i; j++){
+//         if( i % j == 0){
+//             souoc ++;
+//         }  
+//     }
+//     if (souoc == 2){
+//         string = string + " " + i;
+        
+//     }
+// }
+// console.log(string);
+//B4
